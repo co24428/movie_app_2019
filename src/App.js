c1ff1e4d0c9cd496fbc430abf7dd869b19af6331
ihwan <ihwan1236@gmail.com>
2020-01-02 20:29:34 +0900

    react state까지

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food( {name, picture} ) {
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} />
-  </div>
-}
+class App extends React.Component{
+  state ={
+    count: 0
+  };
+  add =() => {
+    console.log("add")
+    this.setState(current => ({count: current.count+1}));
+  };
+  minus =() => {
+    console.log("minus")
+    this.setState(current => ({count: current.count-1}));
+  };
 
-const foodILike = [
-  {
-    name: "Kimchi",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FeTucCw1w6Ak%2Fmaxresdefault.jpg"
-  },
-  {
-    name: "Kimbap",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F5%2F56%2FVegetable_gimbap.jpg%2F1200px-Vegetable_gimbap.jpg"
-  },
-  {
-    name: "Shusi",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F6%2F60%2FSushi_platter.jpg%2F1200px-Sushi_platter.jpg"
-  },
-  {
-    name: "Ramen",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FB8y3SSmz4sg%2Fmaxresdefault.jpg"
-  }
 
-]
-
-function App() {
-  return (
-    <div>
-      <h1>Hello~~~</h1>
-      <Food name="kimchi" />
-      {foodILike.map(dish => (
-        <Food name={dish.name} picture= {dish.image} />
-      ))}
+  render(){
+    // return <h1>The number is {this.state.count}</h1>
+    return <div>
+      <h1>The number is {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>minus</button>
     </div>
-  );
+  };
 }
 
 export default App;
